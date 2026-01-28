4da931bc037ef1379d552d5f763cf72bd4a13e1c
Ezequiel <e-.-zequiel@hotmail.com>
2026-01-28 14:55:45 -0300

    Configurar Api para usar variable de entorno

--- Frontend/src/config/api.js
+++ Frontend/src/config/api.js
@@ -1,6 +1,5 @@
-// Aquí definimos la URL base de tu Backend.
-// Más adelante, puedes cambiar este string por una variable de entorno (ej: import.meta.env.VITE_API_URL)
-const BASE_URL = "http://localhost:3001";
+// definimos la URL base del Backend.
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3001";
 
 export const API_ROUTES = {
     USERS: `${BASE_URL}/users`,
@@ -8,6 +7,4 @@ export const API_ROUTES = {
     COMMENTS: `${BASE_URL}/comments`,
     POST_IMAGES: `${BASE_URL}/postimages`,
     ETIQUETAS: `${BASE_URL}/etiqueta`,
-  // Aquí puedes ir agregando más rutas a medida que crezca tu app
-  // LOGIN: `${BASE_URL}/login`,
 };
